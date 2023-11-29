be950ebe5cfe5f8c06e500e592c4bf228f96d671
Grigorii Matiukhin <contact@gmatiukhin.site>
2023-11-29 21:51:36 +0300

    create a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use pixels::{Pixels, SurfaceTexture};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let width = 512;
+    let height = 512;
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Pixel Renderer")
+        .with_inner_size::<LogicalSize<i32>>((width, height).into())
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut pixels = {
+        let size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(size.width, size.height, &window);
+        Pixels::new(size.width, size.height, surface_texture).unwrap()
+    };
+
+    if let Err(e) = event_loop.run(move |event, elwt| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            elwt.exit();
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Resized(size),
+            ..
+        } => {
+            let size = size.to_logical(window.scale_factor());
+            pixels
+                .resize_surface(size.width, size.height)
+                .expect("Error resizing pixel surface.");
+            pixels
+                .resize_buffer(size.width, size.height)
+                .expect("Error resizing pixel buffer.");
+        }
+        Event::WindowEvent {
+            event: WindowEvent::RedrawRequested,
+            ..
+        } => {
+            // let frame = pixels.frame_mut();
+            pixels.render().expect("Error drawind pixels.");
+        }
+        _ => (),
+    }) {
+        eprint!("Event loop error: {e:?}");
+    }
 }
